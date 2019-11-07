4a8df785759a16372eaa0ddbe8f76c513bb82ba4
rafaelhueb92 <rafaelhueb92@gmail.com>
2019-11-07 19:44:57 -0300

    First commit

---
+++ 2/lists.py
@@ -0,0 +1,13 @@
+mylist = []
+mylist.append(1) # add value 1 to the list
+mylist.append(2)
+mylist.append(3)
+
+# Lista an one index of list
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
---
+++ 4/stringFormats.py
@@ -0,0 +1,21 @@
+# This prints out "Hello, John!"
+name = "John"
+print("Hello, %s!" % name)
+
+# This prints out "John is 23 years old."
+name = "John"
+age = 23
+print("%s is %d years old. %s" % (name, age, name))
+
+"""/
+%s - String (or any object with a string representation, like numbers)
+
+%d - Integers
+
+%f - Floating point numbers
+
+%.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+%x/%X - Integers in hex representation (lowercase/uppercase)
+
+"""
